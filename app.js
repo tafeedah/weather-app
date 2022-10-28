41768396ebbef28a8057ef17edfa7608f454b39a
Ahmad Khalil Jamil <coderkherleelaon@outlook.com>
2022-10-28 10:36:23 +0200

    api

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ window.addEventListener('load', () => {
             long = point.coords.longitude;
             lat = point.coords.latitude;
 
-            //const proxy = "https://cors-anywhere.herokuapp.com/"
-            const api = `https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},${long}`;
+            const proxy = "https://cors-anywhere.herokuapp.com/"
+            const api = `${proxy}api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},${long}`;
             fetch(api)
                 .then(response => {
                     return response.json();
