8b21be6e50f4861ae21a96bb6f89b9b4a5e74606
Ahmad Khalil Jamil <coderkherleelaon@outlook.com>
2022-10-28 14:50:58 +0200

    final

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ window.addEventListener('load', () => {
     let temperatureDescription = document.querySelector('.temperature-description');
     let temperatureDegree = document.querySelector('.temperature-degree');
     let locationTimezone = document.querySelector('.location-timezone');
+    let temperatureSection = document.querySelector('.temperature');
+    const temperatureSpan = document.querySelector('.temperature span');
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(point => {
@@ -20,14 +22,42 @@ window.addEventListener('load', () => {
                 })
                 .then(data => {
                     //console.log(data);
-                    const {temperature, summary} =data.currently;
+                    const {
+                        temperature,
+                        summary,
+                        icon
+                    } = data.currently;
                     //Set DOM Elements from API
                     temperatureDegree.textContent = temperature;
                     temperatureDescription.textContent = summary;
                     locationTimezone.textContent = data.timezone;
+                    
+                    //FORMULA FOR CELSIUS
+                    let celsius = (temperature - 32) * (5 / 9);
+
+                    //Set Icon
+                    setIcons(icon, document.querySelector(".icon"));
+
+                    //Change temperature to Celsius/Farenheit
+                        temperatureSection.addEventListener('click', () =>{
+                            if(temperatureSpan.textContent === "F"){
+                                temperatureSpan.textContent = "C";
+                                temperatureDegree.textContent = Math.floor(celsius);
+                            }else{
+                                temperatureSpan.textContent = "F";
+                                temperatureDegree.textContent = temperature;
+                            }
+                        })
                 });
         });
 
 
+    }
+
+    function setIcons(icon, iconID) {
+        const skycons = new Skycons({color:"white"});
+        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[currentIcon]);
     }
 });
